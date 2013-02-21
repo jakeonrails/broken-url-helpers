2628416a7f9b992431a69a9f1a26e6535701f022
Jake Moffatt <jakeonrails@gmail.com>
2013-02-20 18:06:01 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrokenUrlHelpers::Application.config.secret_token = 'bc7180d3ac9b6068d6c65448e20faa282db54cecc3e59aa10af6cd776f34530c5566e2e639e8aa85095e922f0f7522f77a5ce062a21a2c6d11f64b2a014b000a'
---
+++ spec/features/projects_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'In spec/features' do
+
+  before do
+    Rails.application.routes.default_url_options[:host] = 'www.jakeonrails.com'
+  end
+
+  it 'I should be able to change the URL helper host' do
+    projects_url.should == 'http://www.jakeonrails.com/projects'
+  end
+
+end
